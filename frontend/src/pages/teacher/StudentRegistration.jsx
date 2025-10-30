8a28292be007938ef21ed3759f3b42a4c3584c72
Telesphore Uwabera <t.uwabera@alustudent.com>
2025-10-30 07:23:29 +0200

    Fix StudentRegistration imports: use internal translation hook and default teacherApiService export

--- frontend/src/pages/teacher/StudentRegistration.jsx
+++ frontend/src/pages/teacher/StudentRegistration.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
-import { teacherApiService } from '../../services/teacherApiService';
+import { useTranslation } from '../../lib/language';
+import teacherApiService from '../../services/teacherApiService';
 
 const StudentRegistration = () => {
   const { t } = useTranslation();
